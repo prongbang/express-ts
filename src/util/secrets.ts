6ed14e0eb7fe7a81d5ca74b69e7a9dc4aa8dedd3
prongbang <prongbang@gmail.com>
2020-02-20 23:51:18 +0700

    clean

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -3,14 +3,8 @@ import dotenv from "dotenv";
 import fs from "fs";
 
 if (fs.existsSync(".env")) {
-    logger.debug("Using .env file to supply config environment variables");
     dotenv.config({ path: ".env" });
 }
 export const ENVIRONMENT = process.env.NODE_ENV;
 export const JWT_SECRET = process.env["JWT_SECRET"];
 export const PORT = process.env["PORT"];
-
-if (!JWT_SECRET) {
-    logger.error("No client secret. Set JWT_SECRET environment variable.");
-    process.exit(1);
-}
